d24fc679fc16eeca7a8d3e2c93d52b15903778cf
Lucas Gazzola <lucasgazzola1@outlook.com>
2025-12-13 17:56:55 -0300

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
+  app.enableCors(
+    {
+      origin: '*',
+    }
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
