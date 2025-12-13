bc0a7e8e7868d4dcbfd5af2b9e7d34c006f01fe9
Lucas Gazzola <lucasgazzola1@outlook.com>
2025-12-13 17:56:28 -0300

    Arreglado claims

--- src/claims/dto/update-claim.dto.ts
+++ src/claims/dto/update-claim.dto.ts
@@ -1,4 +1,31 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateClaimDto } from './create-claim.dto';
+import { ClaimStatus } from '../../common/enums/claims.enums';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
-export class UpdateClaimDto extends PartialType(CreateClaimDto) {}
+export class UpdateClaimDto extends PartialType(CreateClaimDto) {
+	@IsOptional()
+	@IsEnum(ClaimStatus)
+	claimStatus?: ClaimStatus;
+
+	@IsOptional()
+	@IsString()
+	actions?: string;
+
+	@IsOptional()
+	@IsString()
+	area?: string;
+
+	@IsOptional()
+	@IsString()
+	subarea?: string;
+
+	@IsOptional()
+	@IsString()
+	project?: string;
+
+
+  @IsOptional()
+  @IsString()
+  finalResolution?: string;
+}
