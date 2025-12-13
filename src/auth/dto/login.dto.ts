21188eb47b46201e9596eafbb8c9d87a85889a25
Lucas Gazzola <lucasgazzola1@outlook.com>
2025-12-13 19:24:32 -0300

    Fix para swagger

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,13 +2,13 @@ import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginAuthDto {
-  @ApiProperty({ example: 'maria@example.com' })
+  @ApiProperty({ example: 'lucas@example.com' })
   @IsNotEmpty({ message: 'El email no puede estar vacío' })
   @IsEmail({}, { message: 'El formato del email no es válido' })
   @IsString()
   email: string;
 
-  @ApiProperty({ example: 'Secret1' })
+  @ApiProperty({ example: 'lucasexample' })
   @IsNotEmpty({ message: 'La contraseña no puede estar vacía' })
   @IsString()
   @MinLength(6)
