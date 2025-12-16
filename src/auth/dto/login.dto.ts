c115e1f6ab6633ea5fd1c7c47e66c493052b84b1
Lucas Gazzola <lucasgazzola1@outlook.com>
2025-12-16 19:03:01 -0300

    Cambiado usuario a uno existente

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,13 +2,13 @@ import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginAuthDto {
-  @ApiProperty({ example: 'lucas@example.com' })
+  @ApiProperty({ example: 'customer1@example.com' })
   @IsNotEmpty({ message: 'El email no puede estar vacío' })
   @IsEmail({}, { message: 'El formato del email no es válido' })
   @IsString()
   email: string;
 
-  @ApiProperty({ example: 'lucasexample' })
+  @ApiProperty({ example: 'Customer1pass' })
   @IsNotEmpty({ message: 'La contraseña no puede estar vacía' })
   @IsString()
   @MinLength(6)
